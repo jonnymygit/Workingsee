fa1eec89fbd74e4d5546070b8e5d1c9140aa2222
jonnymygit <jonnyjames9022@gmail.com>
2025-11-18 13:43:11 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,14 +7,14 @@ API_ID = int(environ.get("API_ID", "29115102"))
 API_HASH = environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "1224092270"))
+OWNER = int(environ.get("OWNER", "7192966134"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '1224092270').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7192966134').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1224092270').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7192966134').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -23,6 +23,7 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
